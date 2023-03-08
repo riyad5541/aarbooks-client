5cb0c4034403eee8f306bb0ca8e8e3153e35ff0d
riyad5541 <abdullahalriyad5541@gmail.com>
2023-03-08 21:18:45 +0600

    login and signup completed

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,86 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Login = () => {
+    const googleProvider = new GoogleAuthProvider();
+    const { register, formState: { errors }, handleSubmit } = useForm()
+    const { signIn, providerLogin } = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
+    const [loginUserEmail, setLoginUserEmail] = useState('');
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/';
+
+    const handleLogin = data => {
+        console.log(data);
+        setLoginError('');
+        signIn(data.email, data.password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate(from, {replace:true});
+                setLoginUserEmail(data.email);
+               
+            })
+            .catch(error => {
+                console.log(error.message)
+                setLoginError(error.message);
+            });
+        
+    };
+
+    const handleGoogleSignIn = () => {
+        providerLogin(googleProvider)
+          .then((result) => {
+            const user = result.user;
+            const googleUser = {
+              name: user.displayName,
+              email: user.email,
+              role: "Buyer",
+            };
+            toast.success("User Created Successfully");
+            // saveUser(googleUser);
+          })
+          .catch((error) => console.log(error));
+      };
     return (
-        <div>
-            this  is login
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 className='text-2xl text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input type="text" {...register("email",{
+                                required: "Email Address is required"
+                            })} 
+                            className="input input-bordered w-full max-w-xs" />
+                            {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input type="password" {...register("password", {
+                                required: "Password is required",
+                                minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                            })}
+                            className="input input-bordered w-full max-w-xs" />
+                        <label className="label"><span className="label-text">Forget Password?</span></label>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                    </div>
+                    <input className='btn btn-accent w-full' value="Login" type="submit" />
+                    <div>
+                        {loginError && <p className='text-red-600'>{loginError}</p>}
+                    </div>
+                </form>
+                <p>New to Resale Books <Link className='text-primary' to="/signup">Create New Account</Link></p>
+                <div className="divider">OR</div>
+                <button onClick={handleGoogleSignIn} className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import Signup from "../../Pages/Login/Signup";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -16,8 +19,16 @@ const router = createBrowserRouter([
                 path:'/login',
                 element:<Login></Login>
             },
+            {
+                path:'/signup',
+                element:<Signup></Signup>
+            },
         ]
-    }
+    },
+    {
+        path:'/dashboard',
+        element:<PrivateRoute><Dashboard></Dashboard></PrivateRoute>
+    },
 ])
 
 export default router;
