da28b73d4a3c44bd892dd28e172469a863501626
riyad5541 <abdullahalriyad5541@gmail.com>
2023-03-09 20:18:15 +0600

    add product

---
+++ src/Pages/Dashboard/AddProducts/AddProducts.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+
+const AddProducts = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleAddProduct = data => {
+        // console.log(data)
+
+        const product = {
+            categoryId:data.categoryId,
+            categoryName:data.categoryName,
+            img:data.img,
+            title:data.title,
+            location:data.location,
+            name:data.name,
+            condition:data.condition,
+            description:data.description,
+            resaleprice:data.resaleprice,
+            originalprice:data.originalprice,
+            yearuse:data.yearuse,
+            mobileNum:data.mobileNum,  
+            postedTime:data.postedTime,
+
+
+        }
+
+        fetch("http://localhost:5000/products", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+              authorization: `bearer ${localStorage.getItem("accessToken")}`,
+            },
+            body: JSON.stringify(product),
+          })
+            .then((res) => res.json())
+            .then((result) => {
+              console.log(result);
+              toast.success(`${data.title} is addded successfully`);
+            //   navigate("/dashboard/myProducts");
+            });
+
+    }
+    return (
+        <div className='w-96 p-7'>
+            <h2 className="text-4xl">Add Product</h2>
+
+            <form onSubmit={handleSubmit(handleAddProduct)}>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">CategoryId</span></label>
+                    <select {...register("categoryId")} className="select select-bordered w-full max-w-xs"
+                    >
+                        <option value="01" selected>01</option>
+                        <option value="02" selected>02</option>
+                        <option value="03" selected>03</option>
+                    </select>
+                    
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Product Name</span></label>
+                    <input type="text" {...register("title", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Resale Price</span></label>
+                    <input type="text" {...register("resaleprice", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Original Price</span></label>
+                    <input type="text" {...register("originalprice", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Condition</span></label>
+                    <input type="text" {...register("condition", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Users Years</span></label>
+                    <input type="text" {...register("yearuse", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Posted Time</span></label>
+                    <input type="text" {...register("postedTime", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Mobile Number</span></label>
+                    <input type="text" {...register("mobileNum", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Location</span></label>
+                    <input type="text" {...register("location", {
+                        required: "Name is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Email</span></label>
+                    <input type="email" {...register("email", {
+                        required: true
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Category Name</span></label>
+                    <select {...register("categoryName")} className="select select-bordered w-full max-w-xs"
+                    >
+                        <option value="Physics" selected>Physics</option>
+                        <option value="Math" selected>Math</option>
+                        <option value="Chemistry" selected>Chemistry</option>
+                    </select>
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Description</span></label>
+                    <input type="text" {...register("description", {
+                        required: true
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Seller Name</span></label>
+                    <input type="text" {...register("name", {
+                        required: true
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label"> <span className="label-text">Photo URL</span></label>
+                    <input type="text" {...register("img", {
+                        required: "Photo is Required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.img && <p className='text-red-500'>{errors.img.message}</p>}
+                </div>
+                <input className='btn btn-accent w-full mt-4' value="Add Product" type="submit" />
+            </form>
+
+        </div>
+    );
+};
+
+export default AddProducts;
---
+++ src/Pages/Dashboard/AddedProduct/AddedProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddedProduct = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddedProduct;
